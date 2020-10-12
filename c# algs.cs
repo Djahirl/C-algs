fcfa7d7f11d83b807fa81eda32f7a111646844a9
Djahirl <72760434+Djahirl@users.noreply.github.com>
2020-10-12 15:20:40 -0300

    Update c# algs.cs

--- c# algs.cs
+++ c# algs.cs
@@ -4,27 +4,28 @@ public class Processo
 {
    public static void Main()
    {
-    
-	//À partir do problema proposto:
-	/*
-	1)  Elaborar um  programa em  C++ que,  inicialmente,  leia  dois números inteiros.
-		Em  seguida,  o programa apresenta um menu de opções (switch...case) que permite escolher os seguintes cálculos,
-		que devem ser mostrados na tela: [1] soma dos números pares contidos no intervalo formado por esses  números 
-		(por  exemplo,  se  o  usuário  digitar  1  e  9  para  início  e  fim  de  intervalo,  o  resultado mostrado na tela será 20, que é a soma de 2+4+6+8);
-	[2] média aritmética entre esses números; [3]soma e o produto desses números. 
-	Dicas para a soma dos números pares: •Crie um laço de repetição com o comando for, 
-	cuja variável de controle está vinculada ao início e fim do intervalo determinado pelo usuário;
-	•Para acumular a soma, crie uma variável soma que dentro do laço de repetição some apenas os números pares.
+    	/*
+	À partir do problema proposto:
 	
+	Elaborar um  programa em  C++ que,  inicialmente,  leia  dois números inteiros.
+		Em  seguida,  o programa apresenta um menu de opções (switch...case) que permite escolher os seguintes cálculos,
+		que devem ser mostrados na tela:
+	1. A soma dos números pares contidos no intervalo formado por esses  números.
+		(por  exemplo,  se  o  usuário  digitar  1  e  9  para  início  e  fim  de  intervalo,  o  resultado mostrado na tela será 20, que é a soma de 2+4+6+8);
+	2. A média aritmética entre esses números; 
+	3. A soma e o produto desses números. 
+	
+	* Dicas para a soma dos números pares: c
+		-Crie um laço de repetição com o comando for, 
+			cuja variável de controle está vinculada ao início e fim do intervalo determinado pelo usuário;
+		-Para acumular a soma, crie uma variável soma que dentro do laço de repetição some apenas os números pares.
 	
 	fontes: 
 	https://docs.microsoft.com/pt-br/dotnet/csharp/language-reference/keywords/if-else
 	https://docs.microsoft.com/pt-br/dotnet/csharp/language-reference/keywords/switch
 
-	por: ;J
+	;J 12.10.20
 	*/
-	
-	
 	
     int op, n1, n2, somn1n2 = 0, prodn1n2;
     double median1n2;
